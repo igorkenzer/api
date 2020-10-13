4d0c1f3fda215283a0debfcb31a0d87a35d83353
kenzer <igor@kenzer.ru>
2020-10-13 08:59:39 +0300

    client start

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,9 +1,16 @@
+//import Axios from 'axios';
+//const messages = Vue.resource('/news{/id}')
 
-const messages = Vue.resource('/news{/id}')
-
-var app = new Vue({
+new Vue({
     el: '#app',
-    data: {
-        message: 'Привет, Vue!'
+    data() {
+        return {
+            info: null
+        };
+    },
+    mounted() {
+        axios
+            .get('http://127.0.0.1:8080/news')
+            .then(response => (this.info = response));
     }
-})
+});
